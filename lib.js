609f72d733ae18e14556426655ae57b05a676cf6
Hansaem <100188539+saem020402@users.noreply.github.com>
2022-04-07 00:56:21 +0900

    Add files via upload

---
+++ lib.js
@@ -0,0 +1,15 @@
+function sum(numbers) {
+    return numbers.reduce((prev, curr) => prev + curr, 0);
+}
+function avg(numbers) {
+    return sum(numbers) / numbers.length;
+}
+1
+function max(numbers) {
+    return numbers.reduce((max, curr) => (max > curr ? max : curr), numbers[0]);
+}
+module.exports = {
+    sum,
+    avg,
+    max,
+};
